a8beb0d0387822040bdfa0790e13c00e8b95b6c7
kiyaan-coder <kiyaanprozz@gmail.com>
2025-04-11 17:58:03 +0530

    complete file

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-# In-memory task list
-tasks = []
+# SQLite database config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Task model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+# Create DB
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
+    tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
 @app.route('/add', methods=['POST'])
 def add():
-    task = request.form.get('task')
-    if task:
-        tasks.append(task)
+    task_content = request.form.get('task')
+    if task_content:
+        new_task = Task(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
     return redirect('/')
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
-    if 0 <= task_id < len(tasks):
-        tasks.pop(task_id)
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
     return redirect('/')
 
 if __name__ == '__main__':
